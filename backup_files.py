d48d936e05d5171d6928b752692872cd37e9b54c
BlueShark100 <bmalbertoni@gmail.com>
2024-11-26 21:36:58 -0700

    Update backup_files.py
    added epic comments to the code

--- backup_files.py
+++ backup_files.py
@@ -1,45 +1,59 @@
-# Import Module
+# Import Modules
 import os, datetime, sys
 
+# this will return all the files and directorys (aka folders) in 
+# the same directory as the program as a list of strings
 files = os.listdir()  # list all the files in the same directory as the program
 
+# try if an argument in the command line was given
 try:
+    # if its a help argument, give the help info and exit
     if sys.argv[1] == 'help' or sys.argv[1] == '-h':
         print("\nTo specify the name of the backup folder type it after calling the program name")
         print(f"for example:\npython {sys.argv[0]} 'name_of_backup folder'")
         print("If you dont name a backup folder name the program will default to 'backups'\n")
         exit()
-    backup_folder = sys.argv[1]
+    # if we kept going, the argument is the name for the backup folder
+    backup_folder = sys.argv[1] 
 except:
+    # no argument? no problem. default the name of the backup folder
     backup_folder = 'backups'
 
 try:
-    os.listdir(backup_folder)
+    os.listdir(backup_folder)  # check if the backup folder exists
 except:
-    os.mkdir(backup_folder)
+    os.mkdir(backup_folder)  # if it doesn't, create it
 
-for file in files[:]:
-    if not os.path.isdir(file):
+for file in files[:]:  # files[:] creates a copy using slicing in python
+    if not os.path.isdir(file):  # if its not a directory
+        # use the linux/unix system command to copy the files over to the backup folder
         os.system(f"cp '{file}' '{backup_folder}/{file}'")
-    else:
+        # os.system is the same as running the command in terminal
+    else:  # if it IS a directory, remove it from the list so we know we didnt copy it
         files.remove(file)
 
-with open(f"{backup_folder}/AA_BACKUP_INFO.txt", 'w') as output:
+with open(f"{backup_folder}/AA_BACKUP_INFO.txt", 'w') as output:  # open the output file in write mode
+    # add some stats to the text file for context when this program was last run
     output.write(f'Last backup occurred at {datetime.datetime.now()}\n')
     output.write(f'Number of files copied: {len(files)}\n')
     output.write(f'List of files:\n')
+    # I made it two rows so it's kinda fancy
     for i in range(0, len(files), 2):
         try:
+            # using files:45 makes the string take up at least 45 characters regardless
+            # using files:<45 (adding the less than sign) makes it left-aligned
             output.write(f'\n{files[i]:<45}\t{files[i+1]}')
         except:
+            # the first one will try two files but if we are at the end
+            # of an odd numbered list of files we only print one
             output.write(f'\n{files[i]:<45}')
 
+# print to terminal what just happened so there's some confirmation
 print(f"\n{len(files)} files succsesfully backed up into '{backup_folder}'")
 
-num_hidden_files = 0
+# below I'm using list comprehension in python
+# Look it up its super cool! 
+hidden_files = [x for x in files if x.startswith(".")]
+# adds all items from the files list if it starts with '.'
 
-for file in files:
-    if file.startswith('.'):
-        num_hidden_files += 1
-
-print(f"{num_hidden_files} files are hidden with '.'\n")
+print(f"{len(hidden_files)} files are hidden with '.'\n")
